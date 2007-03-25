2b7e55231d8b26314b2d1d5225410e60b48a261d
Valentin Hilbig <webmaster@scylla-charybdis.com>
2007-03-25 23:33:02 +0000

    Fixed + better logging

--- script/autostart.sh
+++ script/autostart.sh
@@ -18,19 +18,28 @@
 # Written by Valentin Hilbig <webmaster@scylla-charybdis.com>
 #
 # $Log$
-# Revision 1.1  2007-03-04 02:49:18  tino
-# Commit for dist, see ChanegLog
+# Revision 1.2  2007-03-25 23:33:02  tino
+# Fixed + better logging
 #
+# Revision 1.1  2007/03/04 02:49:18  tino
+# Commit for dist, see ChanegLog
 
 cd || exit
 
 RUNDIR="/var/log/autostart"
 [ -w "$RUNDIR" ] || RUNDIR="/var/tmp/autostart"
-[ -w "$RUNDIR" ] || mkdir -m1777 "$RUNDIR" 2>/dev/null || RUNDIR="/tmp/autostart" && mkdir -m1777 "$RUNDIR" || exit
+[ -w "$RUNDIR" ] || mkdir -m1777 "$RUNDIR" 2>/dev/null || { RUNDIR="/tmp/autostart" && mkdir -m1777 "$RUNDIR"; } || exit
 
 RUNDIR="$RUNDIR/$LOGNAME"
 [ -d "$RUNDIR" ] || mkdir -m700 "$RUNDIR" || exit
 chmod 700 "$RUNDIR" || exit
+
+log()
+{
+lnow="`date +%Y%m%d-%H%M%S` $*"
+echo "$lnow" >> "$RUNDIR/autostart.log"
+echo "$lnow"
+}
 
 ex=0
 for a in autostart/*
@@ -44,9 +53,9 @@ do
 	ptybuffer -l "$RUNDIR/$b.log" -o "$RUNDIR/$b.out" -cf "$RUNDIR/$b.sock" "$a"
 	ret="$?"
 	case "$ret" in
-	0)	echo "`date +%Y%m%d-%H%M%S` startd $b"; [ -z "$1" ] || sleep "$1";;
-	42)	echo "`date +%Y%m%d-%H%M%S` ok $b";;
-	*)	echo "`date +%Y%m%d-%H%M%S` OOPS $b ($ret)" >&2; ex=1;;
+	0)	echo "startd $b"; [ -z "$1" ] || sleep "$1";;
+	42)	echo "ok $b";;
+	*)	echo "OOPS $b ($ret)" >&2; ex=1;;
 	esac
 done
 exit $ex
